01739b5d740d8c35423a215291ca9d74c4966fdf
baidonbo <baidonbo@live.com>
2018-05-06 12:53:17 +0800

    优化响应式布局

--- js/rem.js
+++ js/rem.js
@@ -4,9 +4,38 @@
       recalc = function() {
           var clientWidth = docEl.clientWidth;
           if (!clientWidth) return;
-          docEl.style.fontSize = 8 * (clientWidth / 480) + 'px';
+          docEl.style.fontSize = (8 * (clientWidth / 480) >20 ? 20 : 8 * (clientWidth / 480)) + 'px';
       };
   if (!doc.addEventListener) return;
   win.addEventListener(resizeEvt, recalc, false);
   doc.addEventListener('DOMContentLoaded', recalc, false);
 })(document, window);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// (function(doc, win) {
+//   var docEl = doc.documentElement,
+//       resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+//       recalc = function() {
+//           var clientWidth = docEl.clientWidth;
+//           if (!clientWidth) return;
+//           docEl.style.fontSize = 8 * (clientWidth / 480) + 'px';
+//       };
+//   if (!doc.addEventListener) return;
+//   win.addEventListener(resizeEvt, recalc, false);
+//   doc.addEventListener('DOMContentLoaded', recalc, false);
+// })(document, window);
